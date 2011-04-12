aade0a418b604660f814a41ee18de45648fa3f7c
James Lippitt <james.lippitt@gmail.com>
2011-04-12 15:37:48 +0100

    Implement GPU internal state

--- src/gpu.c
+++ src/gpu.c
@@ -1,6 +1,6 @@
 #include "gpu.h"
 
-struct GPU gpu = {NULL};
+struct GPU gpu = {NULL, 0, 0, 0};
 
 void gpu_reset()
 {
@@ -21,5 +21,77 @@ void gpu_reset()
     SDL_FillRect(gpu.screen, &rect, 0xFFFFFFFF);
 
     SDL_Flip(gpu.screen);
+
+    gpu.mode = 0;
+    gpu.modeclock = 0;
+    gpu.line = 0;
+}
+
+void gpu_step(Word ticks)
+{
+    gpu.modeclock += ticks;
+
+    switch (gpu.mode)
+    {
+        // OAM read mode, scanline active
+        case OAM_MODE:
+            if (gpu.modeclock >= 80)
+            {
+                gpu.modeclock = 0;
+                gpu.mode = VRAM_MODE;
+            }
+            break;
+
+        // VRAM read mode, scanline active
+        // Treat end of mode as end of scanline
+        case VRAM_MODE:
+            if (gpu.modeclock >= 172)
+            {
+                gpu.modeclock = 0;
+                gpu.mode = HBLANK_MODE;
+
+                // TODO: Render scanline
+            }
+            break;
+
+        // After last hblank, update the display
+        case HBLANK_MODE:
+            if (gpu.modeclock >= 204)
+            {
+                gpu.modeclock = 0;
+                gpu.line++;
+
+                if (gpu.line == 143)
+                {
+                    gpu.mode = VBLANK_MODE;
+                    SDL_Flip(gpu.screen);
+                }
+                else
+                {
+                    gpu.mode = OAM_MODE;
+                }
+            }
+            break;
+
+        // Vblank (10 lines)
+        case VBLANK_MODE:
+            if (gpu.modeclock >= 456)
+            {
+                gpu.modeclock = 0;
+                gpu.line++;
+
+                if (gpu.line > 153)
+                {
+                    // Restart scanning modes
+                    gpu.mode = OAM_MODE;
+                    gpu.line = 0;
+                }
+            }
+            break;
+
+        default:
+            // Nothing
+            break;
+    }
 }
 
--- src/gpu.h
+++ src/gpu.h
@@ -7,14 +7,24 @@
 #define VRAM_SIZE 8192
 #define OAM_SIZE  160
 
+#define HBLANK_MODE 0
+#define VBLANK_MODE 1
+#define OAM_MODE    2
+#define VRAM_MODE   3
+
 extern struct GPU
 {
     SDL_Surface *screen;
+    Byte mode;
+    Word modeclock;
+    Byte line;
     Byte vram[VRAM_SIZE];
     Byte oam[OAM_SIZE];
 }
 gpu;
 
 void gpu_reset();
+
+void gpu_step(Word ticks);
 
 #endif
