c08d6cb6b5b355b5194d8450da26f34f513cd6ec
James Lippitt <james.lippitt@gmail.com>
2011-04-12 16:37:07 +0100

    Begin implementing function to draw a scanline to screen

--- src/gpu.c
+++ src/gpu.c
@@ -1,4 +1,16 @@
+#include <stdbool.h>
 #include "gpu.h"
+
+#define BACKGROUND   0x80
+#define SPRITE       0x40
+#define SPRITE_SIZE  0x20
+#define BG_TILE_MAP  0x10
+#define BG_TILE_SET  0x08
+#define WINDOW       0x04
+#define WND_TILE_MAP 0x02
+#define DISPLAY      0x01
+
+static inline bool isset_flag(Byte flag);
 
 static void renderscan();
 
@@ -141,7 +153,21 @@ void gpu_step(Word ticks)
     }
 }
 
+bool isset_flag(Byte flag)
+{
+    return gpu.regs.control & flag;
+}
+
 void renderscan()
 {
+    // Get offset for current tile map
+    Byte *map = gpu.vram + (isset_flag(BG_TILE_MAP) ? 0x1C00 : 0x1800);
+    
+    // Determine line of tiles we're currently using
+    map += (gpu.regs.line + gpu.regs.scrolly) >> 3;
+
+    // Determine which tile in the line to start with
+    map += gpu.regs.scrollx >> 3;
+
 }
 
